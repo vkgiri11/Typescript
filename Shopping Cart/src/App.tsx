a91e9c2f71f2faf2da933c9b208919eddbc63e47
vkgiri <vkgiri11@gmail.com>
2023-08-11 17:24:22 +0530

    Shopping Context Added

--- Shopping Cart/src/App.tsx
+++ Shopping Cart/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 
+import { ShoppingCartProvider } from './context/ShoppingCartContext';
 import Home from './pages/Home';
 import Store from './pages/Store';
 import About from './pages/About';
@@ -8,7 +9,7 @@ import Navbar from './components/Navbar';
 
 const App = () => {
 	return (
-		<>
+		<ShoppingCartProvider>
 			<Navbar />
 			<Container className="mb-4">
 				<Routes>
@@ -17,7 +18,7 @@ const App = () => {
 					<Route path="/about" element={<About />} />
 				</Routes>
 			</Container>
-		</>
+		</ShoppingCartProvider>
 	);
 };
 export default App;
--- Shopping Cart/src/pages/Store.tsx
+++ Shopping Cart/src/pages/Store.tsx
@@ -1,6 +1,20 @@
+import { Col, Row } from 'react-bootstrap';
+
+import storeItems from '../data/items.json';
+import StoreItem from '../components/StoreItem';
+
 const Store = () => {
-  return (
-    <h1>Store</h1>
-  )
-}
-export default Store
+	return (
+		<>
+			<h1>Store</h1>
+			<Row xs={1} md={2} lg={3} className="g-3">
+				{storeItems.map((item) => (
+					<Col key={item.id}>
+						<StoreItem {...item} />
+					</Col>
+				))}
+			</Row>
+		</>
+	);
+};
+export default Store;
